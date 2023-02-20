5fb025abbec3f6968d17c9a2be1941171ffe9860
Jorge Ortiz Leon <jorgeortiz37bbb@gmail.com>
2023-02-20 11:33:57 -0600

    Se modificaron el login, registro y forgot-password

--- src/app/forgot-password/forgot-password.page.ts
+++ src/app/forgot-password/forgot-password.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
+
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.page.scss'],
 })
 export class ForgotPasswordPage implements OnInit {
+  //Form Validables 
+  registerForm: any = FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(public formBuilder: FormBuilder) { }
+
+  //Add user form actions
+  get f() { return this.registerForm.controls; }
+  onSubmit() {
+
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+    //True if all the fields are filled
+    if (this.submitted) {
+      alert("Great!!");
+    }
+
+  }
 
   ngOnInit() {
+    //Add User form validations
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NavController} from '@ionic/angular'
+import { FormControl,FormGroup,Validators,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,31 @@ import {NavController} from '@ionic/angular'
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+    //validaciones del form
+    registerForm:any = FormGroup;
+    submitted = false;
+  constructor(private nav: NavController, private formBuilder: FormBuilder) { }
+  get f() { return this.registerForm.controls; }
 
-  constructor(private nav: NavController) { }
+  onSubmit() {
+  
+    this.submitted = true;
+    // para aqui si el form es invalido
+    if (this.registerForm.invalid) {
+        return;
+    }
+    //True si todos los campos son validos
+    if(this.submitted)
+    {
+      alert("Great!!");
+    }
+  }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
   }
 
   gotoForgotPassword(){
